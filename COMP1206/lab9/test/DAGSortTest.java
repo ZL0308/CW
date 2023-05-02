7e71c39f983ab188e2a4ce9f8b4d0a2948977ce6
Zhengb <zl0319828@gmail.com>
2023-05-02 12:28:24 +0100

    Add some new tests

--- COMP1206/lab9/test/DAGSortTest.java
+++ COMP1206/lab9/test/DAGSortTest.java
@@ -120,7 +120,7 @@ public class DAGSortTest {
   @Test
   void testWithLargeSize() throws InvalidNodeException, CycleDetectedException {
     int[][] edge = new int[1000][1000];       // generate a DAG
-    boolean a = true;
+    boolean a = false;
 
     for (int i = 0; i < 999; i++) {
       edge[i] = new int[]{i + 1};       // from i to i + 1
@@ -137,11 +137,109 @@ public class DAGSortTest {
 
     for (int u = 0; u < edge.length; u++) {       // iterate all the vertex
       for (int v : edge[u]) {                     // iterate the next node of each vertex
-        a = vertexPositions.get(u) < vertexPositions.get(v);   // the value of next node should greater than the vertex
+        if (vertexPositions.containsKey(u) && vertexPositions.containsKey(v)) {
+          a = vertexPositions.get(u) < vertexPositions.get(v);   // the value of next node should be greater than the vertex
+          if (!a) {         // iff one next node smaller than the vertex, the order is error, break
+            break;
+          }
+        }
+      }
+      assertTrue(a);
+    }
+  }
+
+  /**
+   * Test the chain graph
+   *
+   * @throws InvalidNodeException   an exception
+   * @throws CycleDetectedException an exception
+   */
+  @Test
+  void testChain() throws InvalidNodeException, CycleDetectedException {
+    int[][] edges = {{1}, {2}, {3}, {4}, {}};
+    int[] arr = DAGSort.sortDAG(edges);
+
+    int[] excepted = {0, 1, 2, 3, 4};
+
+    assertArrayEquals(arr, excepted);
+  }
+
+  /**
+   * Test the binary tree
+   *
+   * @throws InvalidNodeException   an exception
+   * @throws CycleDetectedException an exception
+   */
+  @Test
+  void testBinaryTree() throws InvalidNodeException, CycleDetectedException {
+    boolean a = false;
+    int[][] tree = {{1, 2}, {3, 4}, {}, {}, {}};
+    int[] arr = DAGSort.sortDAG(tree);
+
+    int[][] expected = {
+            {0, 2, 1, 3, 4},
+            {0, 2, 1, 4, 3},
+            {0, 1, 2, 3, 4},
+            {0, 1, 2, 4, 3},
+            {0, 1, 3, 4, 2},
+            {0, 1, 4, 3, 2},
+            {0, 1, 3, 2, 4},
+            {0, 1, 4, 2, 3}
+    };
+
+    for (int[] arr1 : expected) {
+      if (Arrays.equals(arr, arr1)) {
+        a = true;
+        break;
+      }
+    }
+
+    assertTrue(a);
+  }
+
+  /**
+   * Test a graph with no edge
+   *
+   * @throws InvalidNodeException   an exception
+   * @throws CycleDetectedException an exception
+   */
+  @Test
+  void noEdgeGraph() throws InvalidNodeException, CycleDetectedException {
+    boolean a = false;
+    int[][] graph = {{}, {}, {}};
+    int[] arr = DAGSort.sortDAG(graph);
+
+
+
+    int[][] expected = {
+            {0, 1, 2},
+            {0, 2, 1},
+            {1, 0, 2},
+            {1, 2, 0},
+            {2, 0, 1},
+            {2, 1, 0}
+    };
+
+    for (int[] arr1 : expected) {
+      if (Arrays.equals(arr, arr1)) {
+        a = true;
+        break;
       }
     }
     assertTrue(a);
   }
 
+  @Test
+  void nullElementInGraph(){
+    int[][] edge1 = {{9}, {3}, {3}, null, {5}, {6}, {5}};
+    assertThrows(InvalidNodeException.class, () -> DAGSort.sortDAG(edge1));
+  }
 
+  @Test
+  void singleNode() throws InvalidNodeException, CycleDetectedException {
+    int[][] node = {{}};
+    int[] node1 = DAGSort.sortDAG(node);
+
+    assertEquals(0,node1[0]);
+  }
 }
